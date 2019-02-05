4f6571276e3db5894dc5e42281d638aa4d6b1129
Warren Gray <warren.gray@arcticwolf.com>
2019-02-04 21:58:36 -0500

    Add packaging info.

---
+++ README.md
@@ -0,0 +1,7 @@
+# PyLCOM
+This module calculates the _Lack of Cohesion in Methods_ (LCOM) value for Python files. 
+This value is an objective measure of the [cohesiveness](https://en.wikipedia.org/wiki/Cohesion_(computer_science))
+of a particular class.
+
+Lower values are better, with 0 being the lowest. Higher values indicate that classes
+should be refactored into more cohesive units.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pylcom",
+    version="0.0.1",
+    author="Warren Gray",
+    author_email="warren@warren-gray.com",
+    description="An experiment in calculating LCOM values for Python files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sl5r0/pylcom",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
